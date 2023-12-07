e464565a80e7cababa35dacffb2f62bab5c23ae0
thamidachoudhury1 <106630354+thamidachoudhury1@users.noreply.github.com>
2023-12-07 00:52:24 -0500

    Update ProfileDetails.tsx

--- src/components/ProfileDetails.tsx
+++ src/components/ProfileDetails.tsx
@@ -1,37 +1,105 @@
+"use client";
+import React, {useState, useEffect} from 'react';
+import {doc, setDoc, getDoc} from 'firebase/firestore';
 import Image from 'next/image';
+import ImageAsButton from './ImageAsButton';
+import {collection, query, where} from 'firebase/firestore';
+import {app, firestore} from '../firebase';
 
-const ProfileDetails = () => {
+
+
+const ProfileDetails: React.FC = () => {
+  const [isEditing, setIsEditing] = useState(false);
+  const[name, setName]= useState('John Doe');
+  const[description, setDescription] = useState("Let's Explore");
+
+  const profileRef = doc(collection(firestore, 'Profile'), 'profileName');
+  
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+  const handleSaveClick = () => {
+    setIsEditing(false);
+    setDoc(profileRef, {
+      name, 
+      description,
+    });
+   
+  };
+
+  useEffect(() => {
+    getDoc(profileRef)
+    .then((docSnap) => {
+      if(docSnap.exists()) {
+        const data = docSnap.data();
+        setName(data.name);
+        setDescription(data.description);
+      }
+    })
+    .catch((error) => {
+      console.error('Error fetching document:', error);
+    });
+}, []);
+
   return (
     <section className="max-container padding-container flex flex-col lg:flex-row gap-20 py-10 pb-32 items-start">
       {/* Profile Picture */}
-      <div className='profile-picture relative w-60 h-60 lg:w-60 lg:h-60'>
-        <Image
+      <div className='profile-picture'>
+        <Image 
           src="/pfp.png"
           alt="profile"
+          height={400}
+          width={400}
+        
           
-          objectFit="cover"
-          height={300}
-          width={300}
-
-          objectPosition="top left"
         />
       </div>
 
       {/* Content */}
       <div className='relative z-20'>
         <div className='ml-0 lg:mt-6'>
-        <div className="flex items-center"> {/* Flex container */}
-          <h1 className='bold-52 lg:mt-0'>John Doe</h1>
-            {/* Image */}
-            <img
-             src="\edit-icon-vector-illustration.jpg"
-             alt="Following Image"
-            className="w-6 h-6 ml-30 lg:ml-10" // Adjust size and margins as needed
-        />
-        </div>
-          <p className="regular-16 mt-2 text-gray-30 lg:max-w-[520px]">Let's explore the outdoors together, one snapshot at a time. My passion is photography and would love to collab! #CityEcplorer #PhotographyEnthusiast</p>
-        </div>
+        <div className="flex  flex-col lg:flex-row items-center"> {/* Flex container */}
+        {/* Name input or text */}
+        <div className='flex flex-col'>
+          <h1 className='Name'>
+            {isEditing ? (
+             <input 
+              type="text" 
+              value={name} 
+              onChange={(e) => setName(e.target.value)} 
+              style={{ color: 'black' }}
+              className='name-input'  />
+             ):(
+              name
+             )}
+             </h1>
+        
+        {/* Description */}
+        {!isEditing && (
+          <div className='Description'>
+            <p className='regular-16 mt-2'>
+              {description}
+            </p>
+            </div>
+        )}
 
+        {/* Editable Description */}
+        {isEditing && (
+            <div className='EditableDescription'>
+              <textarea
+                value={description} 
+                onChangeCapture={(e) => setDescription(e.target.value)} 
+                style={{ color: 'black' }} /> 
+                <button className= "EditButton" onClick={handleSaveClick}>Save</button>
+                </div>
+          )}
+          </div>
+
+        {/* Image as a button */}
+        <ImageAsButton handleClick={handleEditClick} />
+        
+          </div>
+        </div>
         {/* Following Count */}
         <div className="flex items-center mt-10 text-gray-30 lg:mt-4">
           <h3 className="bold-12 mr-4">50</h3>
@@ -41,6 +109,7 @@ const ProfileDetails = () => {
         </div>
       </div>
     </section>
+
   );
 };
 
