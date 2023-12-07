5e66bc11789c0ef859e34ae35a63bedeaf5b4095
thamidachoudhury1 <106630354+thamidachoudhury1@users.noreply.github.com>
2023-12-07 02:25:23 -0500

    Update ProfileDetails.tsx

--- src/components/ProfileDetails.tsx
+++ src/components/ProfileDetails.tsx
@@ -7,11 +7,10 @@ import {collection, query, where} from 'firebase/firestore';
 import {app, firestore} from '../firebase';
 
 
-
 export default function ProfileDetails() {
   const [isEditing, setIsEditing] = useState(false);
-  const[name, setName]= useState('John Doe');
-  const[description, setDescription] = useState("Let's Explore");
+  const [name, setName] = useState('John Doe');
+  const [description, setDescription] = useState("Let's Explore");
 
   const profileRef = doc(collection(firestore, 'Profile'), 'profileName');
   
@@ -24,82 +23,101 @@ export default function ProfileDetails() {
       name, 
       description,
     });
-   
   };
 
   useEffect(() => {
     getDoc(profileRef)
-    .then((docSnap) => {
-      if(docSnap.exists()) {
-        const data = docSnap.data();
-        setName(data.name);
-        setDescription(data.description);
-      }
-    })
-    .catch((error) => {
-      console.error('Error fetching document:', error);
-    });
-}, []);
+      .then((docSnap) => {
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          setName(data.name);
+          setDescription(data.description);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching document:', error);
+      });
+  }, []);
 
   return (
     <section className="max-container padding-container flex flex-col lg:flex-row gap-20 py-10 pb-32 items-start">
       {/* Profile Picture */}
-      <div className='profile-picture'>
+      <div style={{
+        borderRadius: '50%',
+        overflow: 'hidden',
+        width: '200px',
+        height: '200px'
+      }}>
         <Image 
           src="/pfp.png"
           alt="profile"
           height={400}
           width={400}
-        
-          
         />
       </div>
 
       {/* Content */}
-      <div className='relative z-20'>
-        <div className='ml-0 lg:mt-6'>
-        <div className="flex  flex-col lg:flex-row items-center"> {/* Flex container */}
-        {/* Name input or text */}
-        <div className='flex flex-col'>
-          <h1 className='Name'>
-            {isEditing ? (
-             <input 
-              type="text" 
-              value={name} 
-              onChange={(e) => setName(e.target.value)} 
-              style={{ color: 'black' }}
-              className='name-input'  />
-             ):(
-              name
-             )}
-             </h1>
+      <div style={{ zIndex: '20' }}>
+        <div className="ml-0 lg:mt-6">
+          <div className="flex  flex-col lg:flex-row items-center"> {/* Flex container */}
+            {/* Name input or text */}
+            <div className="flex flex-col">
+              <h1 style={{
+                fontSize: '50px',
+                paddingBottom: '20px',
+                paddingTop: '0px',
+                paddingRight: '40px',
+                paddingLeft: '0px'
+              }}>
+                {isEditing ? (
+                  <input 
+                    type="text" 
+                    value={name} 
+                    onChange={(e) => setName(e.target.value)} 
+                    style={{ color: 'black' }}
+                    className="name-input"
+                  />
+                ) : (
+                  name
+                )}
+              </h1>
         
-        {/* Description */}
-        {!isEditing && (
-          <div className='Description'>
-            <p className='regular-16 mt-2'>
-              {description}
-            </p>
-            </div>
-        )}
-
-        {/* Editable Description */}
-        {isEditing && (
-            <div className='EditableDescription'>
-              <textarea
-                value={description} 
-                onChangeCapture={(e) => setDescription(e.target.value)} 
-                style={{ color: 'black' }} /> 
-                <button className= "EditButton" onClick={handleSaveClick}>Save</button>
+              {/* Description */}
+              {!isEditing && (
+                <div style={{
+                  color: 'whitesmoke',
+                  fontSize: '20px',
+                  paddingTop: '0px'
+                }}>
+                  <p className="regular-16 mt-2">{description}</p>
                 </div>
-          )}
-          </div>
+              )}
 
-        {/* Image as a button */}
-        <ImageAsButton handleClick={handleEditClick} />
-        
+              {/* Editable Description */}
+              {isEditing && (
+                <div style={{ paddingTop: '10px' }}>
+                  <textarea
+                    value={description} 
+                    onChange={(e) => setDescription(e.target.value)} 
+                    style={{ color: 'black' }} 
+                  /> 
+                  <button style={{
+                    paddingLeft: '20px',
+                    paddingTop: '20px',
+                    fontSize: 'large',
+                    fontWeight: 'bold'
+                  }} onClick={handleSaveClick}>
+                    Save
+                  </button>
+                </div>
+              )}
+            </div>
+
+            {/* Image as a button */}
+            <ImageAsButton handleClick={handleEditClick} />
           </div>
         </div>
+
         {/* Following Count */}
         <div className="flex items-center mt-10 text-gray-30 lg:mt-4">
           <h3 className="bold-12 mr-4">50</h3>
@@ -109,7 +127,5 @@ export default function ProfileDetails() {
         </div>
       </div>
     </section>
-
   );
-};
-
+}
